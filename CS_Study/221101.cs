4211581206099bf27e506302ad14d3795d492c72
ehdbs28 <doyun5373@naver.com>
2022-11-01 11:20:20 +0900

    New Cs file

---
+++ CS_Study/221101.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CS_Study
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
