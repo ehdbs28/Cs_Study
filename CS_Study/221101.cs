cb0b5260f902a7f0d919bfc9c612c3f9f83e76ad
ehdbs28 <doyun5373@naver.com>
2022-11-01 12:57:53 +0900

    2022-11-01(2)
    제네릭으로 바꿈

--- CS_Study/221101.cs
+++ CS_Study/221101.cs
@@ -6,32 +6,28 @@ namespace CS_Study
 {
     class Program
     {   
-        delegate int Comparer(int a, int b);
+        delegate int Comparer<T>(T a, T b);
 
-        static int AscendCompare(int a, int b){
-            if(a > b) return 1;
-            else if(a == b) return 0;
-            else return -1;
+        static int AscendCompare<T>(T a, T b) where T : IComparable{
+            return a.CompareTo(b);
         }
 
-        static int DescendCompare(int a, int b){
-            if(a < b) return 1;
-            else if(a == b) return 0;
-            else return -1;
+        static int DescendCompare<T>(T a, T b) where T : IComparable{
+            return a.CompareTo(b) * -1;
         }
 
-        static void BubbleSort(int[] array, Comparer comparer){
+        static void BubbleSort<T>(T[] array, Comparer<T> comparer){
             for(int i = 0; i < array.Length; i++){
                 for(int j = 0; j < array.Length - (i + 1); j++){
                     if(comparer(array[j], array[j + 1]) > 0){
-                        int temp = array[j];
+                        T temp = array[j];
                         array[j] = array[j + 1];
                         array[j + 1] = temp;
                     }
                 }
             }
 
-            foreach(int i in array){
+            foreach(T i in array){
                 Console.Write($"{i} ");
             }
             Console.Write("\n");
@@ -39,10 +35,20 @@ namespace CS_Study
 
         static void Main(string[] args)
         {
-            int[] array = {5, 3, 7, 9, 1};
-            BubbleSort(array, AscendCompare);
-            int[] array2 = {6, 4, 8, 2 , 10};
-            BubbleSort(array2, DescendCompare);
+            // int[] array = {5, 3, 7, 9, 1};
+            // BubbleSort<int>(array, AscendCompare);
+            // int[] array2 = {6, 4, 8, 2 , 10};
+            // BubbleSort(array2, DescendCompare);
+
+            // char[] charArr = {'b', 'c', 'd', 'a', 'f', 'e', 'g'};
+            // BubbleSort<char>(charArr, AscendCompare);
+            // BubbleSort<char>(charArr, DescendCompare);
+
+            int[] intArr = {5, 3, 4, 6, 1};
+            string[] strArr = {"가나다", "마바사", "아자차"};
+
+            BubbleSort<int>(intArr, AscendCompare);
+            BubbleSort<string>(strArr, DescendCompare);
         }
     }
 }
